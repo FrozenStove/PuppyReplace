cec8d21fb3acc0b2e8898d6d53fa050ab26a7071
FrozenStove <Yessysutter@gmail.com>
2022-07-21 09:06:36 -0700

    It works

--- main.js
+++ main.js
@@ -5,58 +5,153 @@ document.addEventListener('DOMContentLoaded', () => {
     const dog1 = document.querySelector('.dog1');
     const dog2 = document.querySelector('.dog2');
     const dog3 = document.querySelector('.dog3');
-    console.log(dog1)
+    
+    const cat1 = document.querySelector('.cat1');
+    const cat2 = document.querySelector('.cat2');
+    const cat3 = document.querySelector('.cat3');
+    
+    const bird1 = document.querySelector('.bird1');
+    const bird2 = document.querySelector('.bird2');
+    const bird3 = document.querySelector('.bird3');
+    
 
     dog1.addEventListener('click', ReplaceDog1);
 
     function ReplaceDog1() {
-        let dogNum = 0;
-        replaceDog(dogNum)
+        chrome.storage.sync.set({
+            dogNum: 0,
+            keyPhrase1: "dog",
+            keyPhrase2: "Dog"
+        });       
         console.log("Corgi")
     }
 
     dog2.addEventListener('click', ReplaceDog2);
 
     function ReplaceDog2() {
-        let dogNum = 1;
-        replaceDog(dogNum)
+        chrome.storage.sync.set({
+            dogNum: 1,
+            keyPhrase1: "dog",
+            keyPhrase2: "Dog"
+        });        
         console.log('Golden Doodle')
     }
 
     dog3.addEventListener('click', ReplaceDog3);
 
     function ReplaceDog3() {
-        let dogNum = 2;
-        replaceDog(dogNum)
+        chrome.storage.sync.set({
+            dogNum: 2,
+            keyPhrase1: "dog",
+            keyPhrase2: "Dog"
+        });        
         console.log("Chihuahua")
     }
+
+    cat1.addEventListener('click', () => {
+        chrome.storage.sync.set({
+            dogNum: 3,
+            keyPhrase1: "cat",
+            keyPhrase2: "Cat"
+        });        
+        console.log("Cat1")
+    })
+
+    cat2.addEventListener('click', () => {
+        chrome.storage.sync.set({
+            dogNum: 4,
+            keyPhrase1: "cat",
+            keyPhrase2: "Cat"
+        });        
+        console.log("Cat2")
+    })
+
+    cat3.addEventListener('click', () => {
+        chrome.storage.sync.set({
+            dogNum: 5,
+            keyPhrase1: "cat",
+            keyPhrase2: "Cat"
+        });        
+        console.log("Cat3")
+    })
+
+    bird1.addEventListener('click', () => {
+        chrome.storage.sync.set({
+            dogNum: 6,
+            keyPhrase1: "bird",
+            keyPhrase2: "Bird"
+        });        
+        console.log("Bird1")
+    })
+
+    bird2.addEventListener('click', () => {
+        chrome.storage.sync.set({
+            dogNum: 7,
+            keyPhrase1: "bird",
+            keyPhrase2: "Bird"
+        });        
+        console.log("Bird2")
+    })
+
+    bird3.addEventListener('click', () => {
+        chrome.storage.sync.set({
+            dogNum: 8,
+            keyPhrase1: "bird",
+            keyPhrase2: "Bird"
+        });        
+        console.log("Bird3")
+    })
+
 })
 
-// replaceDog(2,document)
 
-// function replaceDog(dognum,document) {
+
+
+
+const dogObj = {};
+
+chrome.storage.sync.get({
+    // the values in here are grabbed as default values
+    dogNum: 0,
+    keyPhrase1: "dog",
+    keyPhrase2: "Dog"
+}, function (items) {
+    // alert('test')
+    console.log(items.dogNum)
+    dogObj.number =  items.dogNum;
+    dogObj.keyPhrase1 = items.keyPhrase1
+    dogObj.keyPhrase2 = items.keyPhrase2
+
+    replaceDog(dogObj);
+}
+
+)
+
+// console.log(chrome.storage.sync)
+function replaceDog (dogObj) {
+
+
 
 const theBody = document.body
 
-
 const bodyText = document.querySelectorAll('h1, h2, h3, h4, h5, h6, p ,li, td, caption, span, table, tr, th, tbody')
 
-let keyPhrase = "dog";
-let keyPhrase1 = "Dog";
-
-let dogNum = 2;
+let keyPhrase1 = dogObj.keyPhrase1
+let keyPhrase2 = dogObj.keyPhrase2
 
 
-const imgArray = ["https://i.pinimg.com/originals/22/fd/1d/22fd1db541a0074b0ea3176814fddf60.jpg", 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMf26GoQeSosYhK9fYrcFYJ55Xq-09bggP0A&usqp=CAU', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F47%2F2021%2F03%2F09%2Fchihuahua-laying-down-wooden-floor-1675701502-2000.jpg',]
+const imgArray = ["https://i.pinimg.com/originals/22/fd/1d/22fd1db541a0074b0ea3176814fddf60.jpg", 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMf26GoQeSosYhK9fYrcFYJ55Xq-09bggP0A&usqp=CAU', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F47%2F2021%2F03%2F09%2Fchihuahua-laying-down-wooden-floor-1675701502-2000.jpg','https://images.unsplash.com/photo-1595433707802-6b2626ef1c91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80','https://images.unsplash.com/photo-1611267254323-4db7b39c732c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2848&q=80','https://images.unsplash.com/photo-1570018144715-43110363d70a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2831&q=80','https://www.tyla.com/cdn-cgi/image/width=720,quality=70,format=jpeg,fit=pad,dpr=1/https%3A%2F%2Fs3-images.tyla.com%2Fs3%2Fcontent%2Fe65975a35ea8fbbf27afdaba1d88a8c1.png','https://www.thesprucepets.com/thmb/QoFOF4pKqGKidE41VMEchaNPRIo=/1080x1080/filters:no_upscale():max_bytes(150000):strip_icc()/79590861_556150255242597_5576742561921620521_n-91402a6c979f429ab5e4554fd6a21f14.jpg','https://www.thesprucepets.com/thmb/h64TESUNS1ed6Ufb86nh-UtiQ2w=/700x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/65822349_609247572918523_5892072578642889552_n-4eeff611681340a79bdff9ac93549aaf.jpg']
 
-let imgLink = imgArray[dogNum]
+let imgLink = imgArray[dogObj.number]
 
-let replacementPhrase = '<img style=" height : 30px" src=' + imgLink + '>'
+let replacementPhrase = '<img style=" height : 40px" src=' + imgLink + '>'
 
 let nReplaced = 0;
 
+// alert('inside the replacements')
+
 for (let i = 0; i < bodyText.length; i++) {
-    if (bodyText[i].innerHTML.includes(keyPhrase) || bodyText[i].innerHTML.includes(keyPhrase1)) {
+    if (bodyText[i].innerHTML.includes(keyPhrase2) || bodyText[i].innerHTML.includes(keyPhrase1)) {
 
 
         if (!bodyText[i].innerHTML.includes("img") || !bodyText[i].innerHTML.includes("</a>")) {
@@ -89,25 +184,11 @@ for (let i = 0; i < bodyText.length; i++) {
             //     }
 
 
-            bodyText[i].innerHTML = bodyText[i].innerHTML.replaceAll(keyPhrase, replacementPhrase)
             bodyText[i].innerHTML = bodyText[i].innerHTML.replaceAll(keyPhrase1, replacementPhrase)
+            bodyText[i].innerHTML = bodyText[i].innerHTML.replaceAll(keyPhrase2, replacementPhrase)
 
         }
     }
 }
 
-
-
-// }
-
-
-// console.log(bodyText[1])
-// console.log(bodyText.length)
-
-
-
-//{/* <img alt="" src="//upload.nni.jpg" decoding="async" width="81" height="64" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/0/05/Lengthy_body_Kanni.jpg/122px-Lengthy_body_Kanni.jpg 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/0/05/Lengthy_body_Kanni.jpg/162px-Lengthy_body_Kanni.jpg 2x" data-file-width="1183" data-file-height="935"></img> */}
-
-
-
-// <a href="/w/index.php?title=Special:CreateAccount&amp;returnto=Domestication+of+the+dog" title="You are encouraged to create an account and log in; however, it is not mandatory"><span>Create account</span></a>
+}
